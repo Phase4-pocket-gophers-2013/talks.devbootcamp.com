5d31a8872ce28547733dc26af1efa5da5b43cc12
Molly Trombley-McCann <molly.kayline@gmail.com>
2013-06-25 15:05:15 -0700

    added a cucumber feature for signing up as a boot, with steps

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130625073134) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "talks", force: true do |t|
     t.string   "title"
     t.text     "content"
